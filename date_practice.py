47763de76c62991288d0195353e91878d6e5d81a
Yujin Chung <yujinchung@Yujins-MacBook-Pro.local>
2019-01-25 13:37:17 +0900

    date_handling

---
+++ date_practice.py
@@ -0,0 +1,4 @@
+import dateutil.parser as p
+print(p.parse('Aug 13, 1997'))
+print(p.parse('2017-03-24'))
+print(p.parse('2018-2-1 15:15'))
---
+++ rsp_game.py
@@ -0,0 +1,10 @@
+"
+<The Topology of the 2x2 Games>
+p.13
+The resulting behaviour is sometimes described as 'myopic' becasue it fails to take into account how other players
+might respond to a given choice..
+
+
+
+
+"
